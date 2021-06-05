1f08cc1603b5e03879a3dee3294100aaedb0bedb
pseudoyu <pseudoyu@connect.hku.hk>
2021-06-05 21:41:54 +0800

    validator implemented for params check

--- controller/user.go
+++ controller/user.go
@@ -18,7 +18,7 @@ func SignUpHandler(c *gin.Context) {
 		// 请求参数有误，直接返回响应
 		zap.L().Error("SignUp with invalid param", zap.Error(err))
 
-		// 用于判断err是不是validator.ValidationErrors类型
+		// 判断err是不是validator.ValidationErrors类型
 		errs, ok := err.(validator.ValidationErrors)
 		if !ok {
 			c.JSON(http.StatusOK, gin.H{
